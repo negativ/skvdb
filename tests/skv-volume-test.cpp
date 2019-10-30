84c706bed317880ad1b598fd6cd16961d13ac29a
Andrew Karlov <dein.negativ@gmail.com>
2019-10-30 19:38:01 +0300

    Volume: open now cached

--- tests/skv-volume-test.cpp
+++ tests/skv-volume-test.cpp
@@ -3,13 +3,15 @@
 #include <gtest/gtest.h>
 
 #include <ondisk/Volume.hpp>
+#include <os/File.hpp>
 
+using namespace skv;
 using namespace skv::ondisk;
 
 namespace {
 #ifdef BUILDING_UNIX
-    const char * const STORAGE_DIR  = "/tmp";
-    const char * const STORAGE_NAME = "test_storage";
+    const std::string STORAGE_DIR  = "/tmp";
+    const std::string STORAGE_NAME = "test_storage";
 #else
     const char * const STORAGE_DIR  = ".";
     const char * const STORAGE_NAME = "test_storage";
@@ -18,6 +20,9 @@ namespace {
 
 TEST(VolumeTest, OpenCloseLink) {
     Volume volume;
+
+    os::File::unlink(STORAGE_DIR + "/" + STORAGE_NAME + ".logd");
+    os::File::unlink(STORAGE_DIR + "/" + STORAGE_NAME + ".index");
 
     auto status = volume.initialize(STORAGE_DIR, STORAGE_NAME);
 
@@ -35,6 +40,21 @@ TEST(VolumeTest, OpenCloseLink) {
         }
         {
             auto [status, procHandle] = volume.link(rootHandle, "proc");
+            ASSERT_TRUE(status.isOk());
+
+            {
+                auto [status, selfHandle] = volume.link(procHandle, "self");
+                ASSERT_TRUE(status.isOk());
+            }
+        }
+        {
+            auto [status, selfHandle] = volume.open("/proc/self");
+
+            ASSERT_TRUE(status.isOk());
+        }
+        {
+            auto [status, selfHandle] = volume.open("/proc/self"); // now with cache hit
+
             ASSERT_TRUE(status.isOk());
         }
     }
