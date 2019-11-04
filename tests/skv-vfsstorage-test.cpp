07ea74392cdbf2c42a89040cea2257c5510fc068
Andrew Karlov <dein.negativ@gmail.com>
2019-11-04 16:16:31 +0300

    Updated test for Storage

--- tests/skv-vfsstorage-test.cpp
+++ tests/skv-vfsstorage-test.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <thread>
 
 #include <gtest/gtest.h>
 
@@ -100,11 +101,14 @@ protected:
         ASSERT_TRUE(storage_.mount(volume1_, "/", "/").isOk());
         ASSERT_TRUE(storage_.mount(volume2_, "/", "/").isOk());
 
-        ASSERT_TRUE(storage_.mount(volume1_, "/a", "/volume1_a").isOk());
+        ASSERT_TRUE(storage_.mount(volume1_, "/a", "/volume1_a", Storage::MaxPriority).isOk());
         ASSERT_TRUE(storage_.mount(volume2_, "/f", "/volume2_f").isOk());
 
         ASSERT_TRUE(storage_.mount(volume1_, "/a/b/c", "/volume1_c").isOk());
-        ASSERT_TRUE(storage_.mount(volume2_, "/f/g/h", "/volume2_h").isOk());
+        ASSERT_TRUE(storage_.mount(volume2_, "/f/g/h", "/volume2_h", Storage::MaxPriority).isOk());
+
+        ASSERT_TRUE(storage_.mount(volume1_, "/a/b/c/d", "/combined").isOk());
+        ASSERT_TRUE(storage_.mount(volume2_, "/f/g/h/i", "/combined", Storage::MaxPriority).isOk());
     }
 
     void doUnmounts() {
@@ -116,6 +120,9 @@ protected:
 
         ASSERT_TRUE(storage_.unmount(volume1_, "/", "/").isOk());
         ASSERT_TRUE(storage_.unmount(volume2_, "/", "/").isOk());
+
+        ASSERT_TRUE(storage_.unmount(volume1_, "/a/b/c/d", "/combined").isOk());
+        ASSERT_TRUE(storage_.unmount(volume2_, "/f/g/h/i", "/combined").isOk());
     }
 
     IVolumePtr volume1_;
@@ -152,6 +159,226 @@ TEST_F(VFSStorageTest, OpenCloseTest) {
 
     doUnmounts();
 }
+
+TEST_F(VFSStorageTest, PropertiesGetSetRemoveTest) {
+    doMounts();
+
+    {
+        auto [status, handle] = volume1_->open("/a/b/c/d");
+
+        ASSERT_TRUE(volume1_->setProperty(handle, "test_int", Property{1024 * 1024}).isOk());
+        ASSERT_TRUE(volume1_->setProperty(handle, "test_str", Property{"First test text"}).isOk());
+        ASSERT_TRUE(volume1_->setProperty(handle, "test_flt", Property{1.0f}).isOk());
+        ASSERT_TRUE(volume1_->setProperty(handle, "test_dbl", Property{123.0}).isOk());
+        ASSERT_TRUE(volume2_->setProperty(handle, "v1_test_dbl", Property{128.32}).isOk());
+
+        ASSERT_TRUE(volume1_->close(handle).isOk());
+    }
+
+    {
+        auto [status, handle] = volume2_->open("/f/g/h/i");
+
+        ASSERT_TRUE(volume2_->setProperty(handle, "test_int", Property{1024 * 1024 * 1024}).isOk());
+        ASSERT_TRUE(volume2_->setProperty(handle, "test_str", Property{"Second test text"}).isOk());
+        ASSERT_TRUE(volume2_->setProperty(handle, "test_flt", Property{100.0f}).isOk());
+        ASSERT_TRUE(volume2_->setProperty(handle, "test_dbl", Property{0.123}).isOk());
+        ASSERT_TRUE(volume2_->setProperty(handle, "v2_test_dbl", Property{64.1}).isOk());
+
+        ASSERT_TRUE(volume2_->close(handle).isOk());
+    }
+
+    {
+        auto [status, handle] = storage_.open("/combined");
+
+        {
+            {
+                auto [status, v] = storage_.hasProperty(handle, "test_int");
+                ASSERT_TRUE(status.isOk());
+                ASSERT_TRUE(v);
+            }
+            {
+                auto [status, v] = storage_.hasProperty(handle, "test_dbl");
+                ASSERT_TRUE(status.isOk());
+                ASSERT_TRUE(v);
+            }
+            {
+                auto [status, v] = storage_.hasProperty(handle, "v2_test_dbl");
+                ASSERT_TRUE(status.isOk());
+                ASSERT_TRUE(v);
+            }
+        }
+
+        {
+            auto [status, properties] = storage_.properties(handle);
+
+            ASSERT_EQ(properties.size(), 6);
+
+            ASSERT_EQ(properties["test_int"], Property{1024 * 1024 * 1024}); // volume 2 has highest priority
+            ASSERT_EQ(properties["test_str"], Property{"Second test text"});
+            ASSERT_EQ(properties["test_flt"], Property{100.0f});
+            ASSERT_EQ(properties["test_dbl"], Property{0.123});
+            ASSERT_EQ(properties["v1_test_dbl"], Property{128.32});
+            ASSERT_EQ(properties["v2_test_dbl"], Property{64.1});
+        }
+
+        ASSERT_TRUE(storage_.setProperty(handle, "combined_property", Property{std::string(1024, 'a')}).isOk());
+
+        {
+            auto [status, handle] = volume1_->open("/a/b/c/d");
+            auto [pstatus, v] = volume1_->hasProperty(handle, "combined_property");
+
+            ASSERT_TRUE(pstatus.isOk());
+            ASSERT_TRUE(v);
+
+            {
+                auto [status, value] = volume1_->property(handle, "combined_property");
+
+                ASSERT_TRUE(status.isOk());
+                ASSERT_EQ(value, Property{std::string(1024, 'a')});
+            }
+
+            ASSERT_TRUE(volume1_->close(handle).isOk());
+        }
+        {
+            auto [status, handle] = volume2_->open("/f/g/h/i");
+            auto [pstatus, v] = volume2_->hasProperty(handle, "combined_property");
+
+            ASSERT_TRUE(pstatus.isOk());
+            ASSERT_TRUE(v);
+
+            {
+                auto [status, value] = volume2_->property(handle, "combined_property");
+
+                ASSERT_TRUE(status.isOk());
+                ASSERT_EQ(value, Property{std::string(1024, 'a')});
+            }
+
+            ASSERT_TRUE(volume2_->close(handle).isOk());
+        }
+
+        ASSERT_TRUE(storage_.close(handle).isOk());
+    }
+
+    {
+        auto [status, handle] = storage_.open("/combined");
+
+        ASSERT_TRUE(storage_.removeProperty(handle, "test_int").isOk());
+        ASSERT_TRUE(storage_.removeProperty(handle, "test_str").isOk());
+        ASSERT_TRUE(storage_.removeProperty(handle, "test_flt").isOk());
+        ASSERT_TRUE(storage_.removeProperty(handle, "test_dbl").isOk());
+        ASSERT_TRUE(storage_.removeProperty(handle, "v1_test_dbl").isOk());
+        ASSERT_TRUE(storage_.removeProperty(handle, "v2_test_dbl").isOk());
+        ASSERT_TRUE(storage_.removeProperty(handle, "combined_property").isOk());
+
+        {
+            auto [status, handle] = volume1_->open("/a/b/c/d");
+
+            auto [pstatus, properties] = volume1_->properties(handle);
+
+            ASSERT_TRUE(pstatus.isOk());
+            ASSERT_TRUE(properties.empty());
+
+            ASSERT_TRUE(volume1_->close(handle).isOk());
+        }
+
+        {
+            auto [status, handle] = volume2_->open("/f/g/h/i");
+
+            auto [pstatus, properties] = volume2_->properties(handle);
+
+            ASSERT_TRUE(pstatus.isOk());
+            ASSERT_TRUE(properties.empty());
+
+            ASSERT_TRUE(volume2_->close(handle).isOk());
+        }
+
+        ASSERT_TRUE(storage_.close(handle).isOk());
+    }
+
+    doUnmounts();
+}
+
+TEST_F(VFSStorageTest, PropertyExpireTest) {
+    using namespace std::chrono;
+    using namespace std::literals;
+
+    doMounts();
+
+    auto [status, handle] = storage_.open("/combined");
+
+    ASSERT_TRUE(storage_.setProperty(handle, "test_int", Property{1024 * 1024}).isOk());
+    ASSERT_TRUE(storage_.setProperty(handle, "test_str", Property{"First test text"}).isOk());
+    ASSERT_TRUE(storage_.setProperty(handle, "test_flt", Property{1.0f}).isOk());
+    ASSERT_TRUE(storage_.setProperty(handle, "test_dbl", Property{123.0}).isOk());
+
+    ASSERT_TRUE(storage_.expireProperty(handle, "test_int", Storage::Clock::now() + 100ms).isOk());
+    ASSERT_TRUE(storage_.expireProperty(handle, "test_str", Storage::Clock::now() + 200ms).isOk());
+    ASSERT_TRUE(storage_.expireProperty(handle, "test_flt", Storage::Clock::now() + 300ms).isOk());
+    ASSERT_TRUE(storage_.expireProperty(handle, "test_dbl", Storage::Clock::now() + 400ms).isOk());
+
+    std::this_thread::sleep_for(150ms);
+
+    {
+        auto [pstatus, v] = volume2_->hasProperty(handle, "test_int");
+
+        ASSERT_TRUE(pstatus.isOk());
+        ASSERT_FALSE(v);
+    }
+
+    std::this_thread::sleep_for(100ms);
+
+    {
+        auto [pstatus, v] = volume2_->hasProperty(handle, "test_str");
+
+        ASSERT_TRUE(pstatus.isOk());
+        ASSERT_FALSE(v);
+    }
+
+    std::this_thread::sleep_for(100ms);
+
+    {
+        auto [pstatus, v] = volume2_->hasProperty(handle, "test_flt");
+
+        ASSERT_TRUE(pstatus.isOk());
+        ASSERT_FALSE(v);
+    }
+
+    std::this_thread::sleep_for(100ms);
+
+    {
+        auto [pstatus, v] = volume2_->hasProperty(handle, "test_dbl");
+
+        ASSERT_TRUE(pstatus.isOk());
+        ASSERT_FALSE(v);
+    }
+
+    {
+        auto [status, handle] = volume1_->open("/a/b/c/d");
+
+        auto [pstatus, properties] = volume1_->properties(handle);
+
+        ASSERT_TRUE(pstatus.isOk());
+        ASSERT_TRUE(properties.empty());
+
+        ASSERT_TRUE(volume1_->close(handle).isOk());
+    }
+
+    {
+        auto [status, handle] = volume2_->open("/f/g/h/i");
+
+        auto [pstatus, properties] = volume2_->properties(handle);
+
+        ASSERT_TRUE(pstatus.isOk());
+        ASSERT_TRUE(properties.empty());
+
+        ASSERT_TRUE(volume2_->close(handle).isOk());
+    }
+
+    ASSERT_TRUE(storage_.close(handle).isOk());
+
+    doUnmounts();
+}
+
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
