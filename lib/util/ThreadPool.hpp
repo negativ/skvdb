fa1d8431da6c7675b2f1d9041faa54933f4f7e78
Andrew Karlov <dein.negativ@gmail.com>
2019-11-06 17:16:00 +0300

    Simple ThreadPool class

---
+++ lib/util/ThreadPool.hpp
@@ -0,0 +1,183 @@
+#pragma once
+
+#include <algorithm>
+#include <atomic>
+#include <future>
+#include <thread>
+#include <vector>
+
+#include <boost/lockfree/queue.hpp>
+
+#include "util/Status.hpp"
+
+namespace skv::util {
+
+class ThreadPool final {
+    struct Callable {
+        Callable() noexcept = default;
+        virtual ~Callable() noexcept = default;
+
+        virtual void call() = 0;
+    };
+
+    class CallWrapper {
+        template <typename F>
+        class Call final: public Callable {
+        public:
+            Call(F&& f):
+                func_{std::move(f)}
+            {}
+
+            ~Call() noexcept override = default;
+
+            void call() override {
+                func_();
+            }
+
+        private:
+            F func_;
+        };
+
+    public:
+        CallWrapper() noexcept  = default;
+
+        template <typename F>
+        CallWrapper(F&& f):
+            impl_{new Call{std::move(f)}}
+        {}
+
+        ~CallWrapper() noexcept = default;
+
+        CallWrapper(const CallWrapper&) = delete;
+        CallWrapper& operator=(const CallWrapper&) = delete;
+
+        CallWrapper(CallWrapper&& other) noexcept {
+            using std::swap;
+
+            swap(impl_, other.impl_);
+        }
+
+        CallWrapper& operator=(CallWrapper&& other) noexcept {
+            using std::swap;
+
+            swap(impl_, other.impl_);
+
+            return *this;
+        }
+
+        void operator()() {
+            impl_->call();
+        }
+
+    private:
+        mutable std::unique_ptr<Callable> impl_;
+    };
+
+public:
+    ThreadPool(std::size_t nThreads = std::thread::hardware_concurrency()):
+        done_{false},
+        joiner_{threadPool_}
+    {
+        if (nThreads == 0)
+            nThreads = std::thread::hardware_concurrency();
+
+        try {
+            std::generate_n(std::back_inserter(threadPool_),
+                            nThreads,
+                            [this] { return std::thread(&ThreadPool::routine, this); });
+        }
+        catch (...) {
+            markDone();
+        }
+    }
+
+    ~ThreadPool() noexcept {
+        markDone();
+
+        while (!taskQueue_.empty()) {
+            CallWrapper *wrapper;
+
+            if (taskQueue_.pop(wrapper))
+                delete wrapper;
+        }
+    }
+
+    template <typename F>
+    decltype(auto) schedule(F&& f) {
+        using result_t = std::invoke_result_t<F>;
+
+        std::packaged_task<result_t()> packagedTask{std::move(f)};
+        auto future{packagedTask.get_future()};
+
+        CallWrapper *task = new CallWrapper(std::move(packagedTask));
+
+        while (!taskQueue_.push(task));
+
+        return future;
+
+    }
+
+    [[nodiscard]] bool done() const noexcept {
+        return done_.load(std::memory_order_acquire);
+    }
+
+private:
+    using container_type = std::vector<std::thread>;
+
+    class Joiner final {
+    public:
+        Joiner(container_type& threads):
+            threads_{threads}
+        {}
+
+        ~Joiner() {
+            std::for_each(std::begin(threads_), std::end(threads_),
+                          [](auto& t) {
+                              if (t.joinable())
+                                  t.join();
+                          });
+        }
+
+    private:
+        container_type& threads_;
+    };
+
+    void markDone() {
+        done_.store(true, std::memory_order_release);
+    }
+
+    void routine() {
+
+        while (!done()) {
+            auto [status, task] = nextTask();
+
+            if (!status.isOk())
+                std::this_thread::yield();
+            else
+                task();
+        }
+    }
+
+    std::tuple<Status, CallWrapper> nextTask() {
+        CallWrapper* task;
+
+        if (taskQueue_.pop(task)) {
+            auto ret = std::move(*task);
+
+            delete task;
+
+            return {Status::Ok(), ret};
+        }
+
+        return {Status::InvalidOperation("Task queue empty"), CallWrapper{}};
+    }
+
+    std::atomic<bool> done_;
+    std::vector<std::thread> threadPool_;
+    Joiner joiner_;
+    boost::lockfree::queue<CallWrapper*,
+                           boost::lockfree::fixed_sized<true>,
+                           boost::lockfree::capacity<2048>> taskQueue_;
+};
+
+}
